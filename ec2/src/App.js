9b00402b3b330ebc887523cd423cbf4aabe0e216
Luchen2001 <luczhou@student.unimelb.edu.au>
2023-04-05 22:51:13 +1000

    test2

--- ec2/src/App.js
+++ ec2/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import thanh from './thanh.jpg'
 function App() {
   return (
     <div>
       <h1>Stu.. Stupid Thanh</h1>
+      <img src = {thanh}/>
     </div>
+
   );
 }
 
